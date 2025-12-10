2fd5e15b706f9ed2401f0680d8888ad8072489e1
Peliah <pelepoupa@gmail.com>
2025-12-10 02:46:47 +0100

    feat: created the analytics page with the charts

---
+++ components/dashboard/analytics/index.ts
@@ -0,0 +1,7 @@
+export { AnalyticsContent } from "./analytics-content";
+export { TourSelector } from "./tour-selector";
+export { AnalyticsStats } from "./analytics-stats";
+export { AnalyticsChart } from "./analytics-chart";
+export { StepDropoffChart } from "./step-dropoff-chart";
+export { AnalyticsEvents } from "./analytics-events";
+
